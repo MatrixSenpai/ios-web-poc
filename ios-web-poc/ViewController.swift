61c39fb9b6eff72544f3174353ce7f1823a9e940
Abhishek Vadnerkar <abhishek@enovlab.com>
2022-10-28 11:59:23 -0400

    Connecting to seatmap

--- ios-web-poc/ViewController.swift
+++ ios-web-poc/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+func delay(_ delay: Double = 1, closure: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay) {
+        closure()
+    }
+}
+
+class ViewController: UIViewController, WKNavigationDelegate {
 
     @IBOutlet weak var webView: WKWebView!
 
@@ -16,18 +22,37 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // Load in an html file written locally
-        let htmlFile = Bundle.main.url(forResource: "test-file", withExtension: "html")!
-        webView.loadFileURL(htmlFile, allowingReadAccessTo: htmlFile)
-        let request = URLRequest(url: htmlFile)
-        webView.load(request)
+//        let htmlFile = Bundle.main.url(forResource: "test-file", withExtension: "html")!
+//        webView.loadFileURL(htmlFile, allowingReadAccessTo: htmlFile)
+//        let request = URLRequest(url: htmlFile)
+//        webView.load(request)
 
-        webView.configuration.userContentController.add(self, name: "ios_buttonTapped")
+        var urlRequest = URLRequest(url: URL(string: "http://yoop-web-av-seatmap-3.oak.dev.yoop.app/seating/seatingChart")!)
+        webView.load(urlRequest)
+        webView.navigationDelegate = self
+        webView.configuration.userContentController.add(self, name: "seatmapClient")
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        delay(0.5) {
+            webView.evaluateJavaScript("window.postMessage({\"name\":\"setMessagePort\", \"params\": {\"webkitHandlerName\": \"seatmapClient\"}})")
+            webView.evaluateJavaScript("window.postMessage(\(configureString))")
+        }
+
+    }
+}
+
+struct Callback {
+    var name: String
+
+    struct Param {
+        var name: String
     }
 }
 
 extension ViewController: WKScriptMessageHandler {
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if message.name == "ios_buttonTapped" {
+        if message.name == "seatmapClient" {
             print("Button tapped: \(message.body)")
         }
     }
